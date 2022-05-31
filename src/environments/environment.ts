b5d1398ba348972d583b8aab5960530cf1dbb267
mastinovts <mastinovts@gmail.com>
2022-05-31 03:25:51 +0200

    pre-final commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    pathApi: 'https://epicode.online/epicodebeservice_v2',
+    adminToken:
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY1MzYzNTE1MSwiZXhwIjoxOTY4OTk1MTUxfQ.TbqmVvOUiGSeqtvLAfKz82S3dNfVALe9VhoUZkH4QA6aDfdUGKOd0xN-JSnLxWbNpjFX5zMAxr4Rdz8inxS8qg',
+    adminTenant: 'fe_0222b',
+};
